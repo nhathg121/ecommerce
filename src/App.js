1ea5e7c1a9da0f8755149c9d8edfd2b97d14b3fe
nhathg99 <nhathg99@gmail.com>
2024-09-03 23:47:29 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <>
+      <h1 className='text-danger'>Ecommerce Website in React JS</h1>
+    </>
+  );
+}
+
+export default App;
