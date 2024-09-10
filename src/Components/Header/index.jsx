38a87fdcc4cf707695327cbe3c8c4bc9e6d473be
nhathg121 <nhathg99@gmail.com>
2024-09-11 00:20:51 +0700

    next section

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,9 @@
 import Logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+import { IoSearch } from "react-icons/io5";
+import { LuUser2 } from "react-icons/lu";
 
 import CountryDropdown from "../CountryDropdown";
 
@@ -16,7 +20,7 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="header">
+        <header className="header">
           <div className="container">
             <div className="row align-items-center justify-content-center">
               <div className="logoWrapper col-sm-2">
@@ -26,16 +30,24 @@ const Header = () => {
               </div>
               <div className="col-sm-10 d-flex align-items-center part2">
                 <CountryDropdown />
-                  
                 {/* Header Search Start Here*/}
-                
-                
-                
+                <div className="headerSearch ml-3 mr-3">
+                  <input type="text" placeholder="Seacrh for product ..." />
+                  <Button>
+                    <IoSearch />
+                  </Button>
+                </div>
                 {/* Header Search End Here*/}
+                <div className="part3 d-flex align-items-center ml-auto">
+                  <Button className="circle">
+                    <LuUser2 />
+                    <p>$650</p>
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </header>
       </div>
     </div>
   );
